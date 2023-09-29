041edcb37b6fc1acecc91dcb7d79d04e2bb70b18
steven <hhuang@ban-qu.com>
2023-09-29 17:05:42 +0800

    finished register and login logic

--- trading_sharing_app/users/urls.py
+++ trading_sharing_app/users/urls.py
@@ -1,6 +1,8 @@
-from .views import register
+from .views import register, user_login, home
 from django.urls import path
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login', user_login, name='login'),
+    path('', home, name='home')
 ]
